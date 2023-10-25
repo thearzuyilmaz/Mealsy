275fbe4f48789c993f3c511f94cf8ae1071c3544
thearzuyilmaz <the.arzu.yilmaz@gmail.com>
2023-10-25 15:02:21 +0200

    Hero Animation Added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -37,12 +37,15 @@ class MealItem extends StatelessWidget {
         child: Stack(
           children: [
             // kTransparentImage comes from the transparent package
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id, // this can be anything but should be unique
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
